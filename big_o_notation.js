89f7ee7c321f4b3cf37a60126cf5ec0597f47d8f
Edgar GC <Edgar.GalvanCuesta@gmail.com>
2021-01-06 14:39:48 -0600

    completed obj and array's big O

--- big_o_notation.js
+++ big_o_notation.js
@@ -51,5 +51,74 @@ strings require O(n) space (where n is  the string length)
 reference types are generally O(n) aka array's or objects
 
 
+====================================================================================================
+*/
+
+/********** Understanding how obj and arr work in Big O **********/
+//why is it costly to add elements to the beginning of an array
+
+/*
+object's work well when you don't need order. quick insertions and removals
+
+Insertion O(1)
+Removal O(1)
+Searching O(N)
+Access O(1)
+*/
+
+let instructor = {
+    firstName: "Kelly",
+    isInstructor: true,
+    favoriteNumbers: [1,2,3,4]
+}
+
+//js is able to add and retrieve and remove in constant time. 
+/*
+Object.keys O(N)
+Object.values O(N)
+Object.entries O(N)
+hasOwnProperty O(1)
 
 */
+
+
+
+// When to use Array's 
+/*
+when you need order, and need fast access and insertion. 
+
+removal gets complicated
+
+insertion - it depends
+removal - it depends
+searching - O(N)
+access - O(1)
+*/
+
+let names = ["Michael", "Melissa", "Andrea"]
+
+// "Michael"  "Melissa"  "Andrea"
+//    0          0           0
+
+/* 
+accessing a value, with a valid index, is constant time. it goes straight to that point in the array. 
+
+Adding a value at the end like normal is O(1) aka pushing to an array,
+but adding at the beginning requires the entire array to re-index everything.
+
+Same thing goes when you are removing from the begining of the array,
+this requires the array to be re-indexes. 
+
+push and pop is always faster than shift and unshift
+
+
+*/
+
+
+
+
+
+
+
+
+
