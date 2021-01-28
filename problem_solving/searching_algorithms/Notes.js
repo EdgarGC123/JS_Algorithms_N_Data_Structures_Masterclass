49b938e15448ae0550041f9021fa80fdf0f77e5e
Edgar GC <Edgar.GalvanCuesta@gmail.com>
2021-01-28 14:04:59 -0600

    organized folders a bit more, started some searching algorithms to mix things up a bit

---
+++ problem_solving/searching_algorithms/Notes.js
@@ -0,0 +1,9 @@
+// Goals
+
+/*
+-Describe what a searching algorithm is.
+-Implement linear search on arrays
+-Implement binary search on sorted arrays
+-Implement a naive string search algorithm
+-implement the KMP string searching algorithm
+*/
